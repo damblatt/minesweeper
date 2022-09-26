15299af3c3ab55a4ba54452bf8b593bf68ab106d
damblatt <dam.blatt@gmail.com>
2022-09-26 08:27:35 +0200

    created file

---
+++ minesweeper/ConsoleHelper.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace minesweeper
+{
+    internal class ConsoleHelper
+    {
+        /// <summary>
+        /// ReadInt
+        /// </summary>
+        /// <param name="min">Minimum value</param>
+        /// <param name="instructionMessage">Message if the user's input is invalid</param>
+        /// <returns></returns>
+        public int ReadInt(int min, string instructionMessage = "")
+        {
+            string? input;
+            int n;
+            bool criteria;
+            while (criteria = (!Int32.TryParse(input = Console.ReadLine(), out n) || n < min))
+            {
+                if (instructionMessage == "" || criteria) { instructionMessage = $"{input} is an invalid number.\nPlease enter a valid number: "; }
+                Console.Write(instructionMessage);
+            }
+            return n;
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="min">Minimum value</param>
+        /// <param name="max">Maximum value</param>
+        /// <param name="instructionMessage">Message if the user's input is invalid</param>
+        /// <returns></returns>
+        public int ReadIntMax(int min, int max, string instructionMessage = "")
+        {
+            string? input;
+            int n;
+            bool criteria;
+            while (criteria = (!Int32.TryParse(input = Console.ReadLine(), out n) || n < min || n >= max))
+            {
+                if (instructionMessage == "" || criteria) { instructionMessage = $"{input} is an invalid number.\nPlease enter a valid number ({min} - {max - 1}): "; }
+                Console.Write(instructionMessage);
+            }
+            return n;
+        }
+    }
+}
---
+++ minesweeper/Introduction.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace minesweeper
+{
+    internal class Introduction
+    {
+        public static ConsoleHelper helper = new ConsoleHelper();
+        public static void IntroMenu()
+        {
+            Console.WriteLine("Welcome to Minesweeper");
+            Console.WriteLine("How many columns/rows should the playing field contain? Enter a natural number between 8 and 26.");
+            helper.ReadIntMax(8, 27);
+        }
+    }
+}
