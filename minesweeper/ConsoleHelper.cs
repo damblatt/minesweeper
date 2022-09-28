6d8b5bc793ddb4be0d5e94f37dd38fdebe9ca4e2
damblatt <dam.blatt@gmail.com>
2022-09-28 09:06:49 +0200

    set neighbourfields, check for bombs around every field

--- minesweeper/ConsoleHelper.cs
+++ minesweeper/ConsoleHelper.cs
@@ -88,12 +88,11 @@ namespace minesweeper
 
         public bool VerifyCoordinate(int y, int x, int gridSize)
         {
-            bool isVerified;
             if (y < 0 || y > gridSize || x < 0 | x > gridSize)
             {
-                return isVerified = false;
+                return false;
             }
-            return isVerified = true;
+            return true;
         }
     }
 }
