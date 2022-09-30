e3378aac185842747bc67752ab94119753117df0
damblatt <dam.blatt@gmail.com>
2022-09-30 12:35:42 +0200

    push

--- minesweeper/Program.cs
+++ minesweeper/Program.cs
@@ -1,15 +1,21 @@
 ﻿
+using System.Media;
+
 namespace minesweeper
 {
     internal class Program
     {
         static void Main(string[] args)
         {
+            var intro = new SoundPlayer("Resources/gameStart.wav");
+            intro.PlaySync();
             Game();
         }
 
         public static void Game()
         {
+            var main = new SoundPlayer("Resources/main.wav");
+            main.PlayLooping();
             Console.Title = "Minesweeper";
             var startScreen = new StartScreen();
             startScreen.PrintStartScreen();
