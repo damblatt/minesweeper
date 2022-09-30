67bb99f29d3443cec1388f63d55e6937b2b03554
damblatt <dam.blatt@gmail.com>
2022-09-30 08:50:27 +0200

    fixed errors

--- minesweeper/Timer.cs
+++ minesweeper/Timer.cs
@@ -11,23 +11,39 @@ namespace minesweeper
         private static DateTime begin;
 
         public static bool timerIsRunning { get; set; }
-       
+
 
         public static void timerStarter()
         {
-                begin = DateTime.Now;
-                timerIsRunning = true;
+            begin = DateTime.Now;
+            timerIsRunning = true;
         }
 
         public static void printTimer()
         {
             var duration = DateTime.Now - begin;
             Console.WriteLine($"Time needed until the start of this turn: {duration.ToString("mm")} minutes {duration.ToString("ss")} seconds");
+            if (duration.ToString("mm") == "00")
+            {
+                Console.WriteLine($"Time needed until the start of this turn: {duration.ToString("ss")} seconds");
+            }
+            else
+            {
+                Console.WriteLine($"Time needed until the start of this turn: {duration.ToString("mm")} minutes {duration.ToString("ss")} seconds");
+            }
         }
         public static void printTimerGameOver()
         {
             var duration = DateTime.Now - begin;
             Console.WriteLine($"\tIn total you needed: {duration.ToString("mm")} minutes {duration.ToString("ss")} seconds");
+            if (duration.ToString("mm") == "00")
+            {
+                Console.WriteLine($"\tIn total you needed: {duration.ToString("ss")} seconds");
+            }
+            else
+            {
+                Console.WriteLine($"\tIn total you needed: {duration.ToString("mm")} minutes {duration.ToString("ss")} seconds");
+            }
         }
     }
 }
